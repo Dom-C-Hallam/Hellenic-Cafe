5c371cca330591adc0209b14126f3832e5b6f83b
Dom-C-Hallam <dominic.hallam.code@gmail.com>
2024-11-26 21:23:03 +0000

    Javascript complete for the cart function, the CSS requires attention due to the placement on the screen

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let carts = document.querySelectorAll('.add-cart');
 let products =[
     {
         name: 'Baklava',
-        tag: 'baklavabrown',
+        tag: 'baklava_brown',
         price: 4.50,
         inCart: 0,
     },
@@ -83,6 +83,7 @@ let products =[
 for (let i=0; i < carts.length; i++) {
     carts[i].addEventListener('click', () =>{
         cartNumbers(products[i]);
+        totalCost(products[i]);
     })
 }
 
@@ -134,4 +135,54 @@ function setItems(product) {
     localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
 
+function totalCost(product) {
+    let cartCost = localStorage.getItem('totalCost'); 
+
+    console.log("My cartCost is", cartCost); 
+    console.log(typeof cartCost );
+    if (cartCost !== null) { 
+        
+        cartCost = parseFloat(cartCost); localStorage.setItem("totalCost", cartCost + product.price); 
+
+        } else { localStorage.setItem("totalCost", product.price); 
+
+        } 
+    }
+
+function displayCart() {
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector(".products");
+    let cartCost = localStorage.getItem('totalCost');
+
+    console.log(cartItems);
+    if ( cartItems && productContainer) {
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <div class="product">
+                <img src="images/${item.tag}.jpg">
+                <span>${item.name}</span>
+            </div>
+            <div class="price">£${item.price}</div>
+            <div class="quantity">${item.inCart}</div>
+            <div class="total">£${item.inCart * item.price}</div>
+            `;
+        });
+
+        productContainer.innerHTML += `
+                <div class="basketTotalContainer>
+                    <h4 class="basketTotalTitle>
+                        Basket Total
+                    </h4>
+                    <h4 class="basketTotal">
+                        £${cartCost}
+                    </h4>
+        `;
+
+    }
+}
+
+
 onLoadCartNumbers();
+displayCart();
